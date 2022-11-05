b32b919edb2a64e8a8d6a566f9764e2deebe875e
ainmm00 <munnie2pm@gmail.com>
2022-11-05 16:17:44 +0800

    Update Recaptcha Key

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 import os
-from flask import Flask
+from flask import Flask, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mysqldb import MySQL
 from flask_mail import Mail
 from random import randint
+from datetime import timedelta
 
 
 base = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=30)
 app.config.from_object('app.config.Config')
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root' 
@@ -18,17 +20,16 @@ app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'gadgetsnow'
 
 # CAPTCHA
-app.config["RECAPTCHA_PUBLIC_KEY"] = "6Lf6IroiAAAAACdelIAKuPCl-DwfF6XcC_BZpTC0"
-app.config["RECAPTCHA_PRIVATE_KEY"] = "6Lf6IroiAAAAAGKFOtWU5skNH7-QjQ4Fg9jWQ-BK"
+app.config["RECAPTCHA_PUBLIC_KEY"] = "6Lcwf94iAAAAADjBmVbvE9GE95o6FAxFiVx2cZHm"
+app.config["RECAPTCHA_PRIVATE_KEY"] = "6Lcwf94iAAAAACU_2RY92W8FTJCQMfszmzuyZa8e"
+
 
 # Email Verification
 mail= Mail(app)
 app.config["MAIL_SERVER"]='smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USERNAME"]='gadgetsnow3103@gmail.com'
-app.config['MAIL_PASSWORD']='texgdnpwcxxpjbhs'   
-# app.config['MAIL_PASSWORD']='dwuxiliefpxpmjrq'   
-
+app.config['MAIL_PASSWORD']='texgdnpwcxxpjbhs' 
 app.config['MAIL_DEFAULT_SENDER'] = 'GadgetsNow'                 
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
@@ -54,9 +55,12 @@ app.register_blueprint(auth, url_prefix='/')
 app.config['STRIPE_PUBLIC_KEY'] = 'pk_test_51LudtbH9RvLfX3o6FSoGYQxQisM9hz1chTbZl761XDY4HhHcaEWYpKtIXZmqjmm6zSTVuXQpISLh0Q8ZJTqDciby002Py4s5oS'
 app.config['STRIPE_SECRET_KEY'] = 'sk_test_51LudtbH9RvLfX3o6zVrWW3RfRkvJjWPNOWzghcgRpr6jxjpWOXKYfi7aVJFjENmA06YKrENcwJOthOWQKhNKK9fU00lpcXpJsJ'
 
+
 # Set up SQLITE - Temp***
 @app.before_first_request
 def init_db():
     sa.create_all()
+    
 
-from app import views, auth, models
+from app import views, auth
+
