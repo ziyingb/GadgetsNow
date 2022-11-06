1250089bd54ee3c45361b725df8f8f300432e458
whitneytwh <whitneytwh14@gmail.com>
2022-11-06 13:10:30 +0800

    Updated tests.py

--- tests.py
+++ tests.py
@@ -1,26 +1,19 @@
 from os import name
 import unittest
+from app.models import LoginForm
 from app import app
 import secrets
 
 class TestHello(unittest.TestCase): 
-    def setUp(self): 
-        app.testing = True 
-        self.app = app.test_client() 
-        app.secret_key = secrets.token_urlsafe(16) 
+    def test_product_information(self):
+
+    # GIVEN a User model
+    # WHEN a new User is created
+    # THEN check the username fields are defined correctly
  
-    def test_landing(self): 
-        rv = self.app.get('/') 
-        self.assertEqual(rv.status, '200 OK') 
- 
-    def test_register_login_submit(self): 
-        #test signup 
-        rv = self.app.post('/register', data= { 
-            "username": "test", 
-            "email": "test@test.com",
-            "password": "test"
-        }) 
-        assert('{"status": "Registration successful"}' in rv.data.decode("utf-8")) 
+        login = LoginForm ('testing@gmail.com', 'testingpassword')
+        assert login.usernameoremail == 'testing@gmail.com'
+        assert login.password == 'testingpassword'
  
 if name == '__main__':
     unittest.main()
